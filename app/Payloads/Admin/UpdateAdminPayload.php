f9c46a205916a7dfd3d14285b596a8afad3b28fb
Ifechi <j.cyxtus@gmail.com>
2024-02-22 08:34:21 +0000

    Added user type to user

--- app/Payloads/Admin/UpdateAdminPayload.php
+++ app/Payloads/Admin/UpdateAdminPayload.php
@@ -4,15 +4,20 @@ declare(strict_types=1);
 
 namespace App\Payloads\Admin;
 
+use App\Enums\UserType;
 use App\Enums\AdminStatus;
+use App\Traits\KeyTransformer;
 
 final readonly class UpdateAdminPayload
 {
+    use KeyTransformer;
+
     public function __construct(
         public int|string $id,
         public string $name,
         public string $email,
         public null|string|AdminStatus $status,
+        public null|string|UserType $userType,
     ) {
     }
 
@@ -20,12 +25,13 @@ final readonly class UpdateAdminPayload
     {
         $data = get_object_vars($this);
 
-
         if (is_string($this->status)) $data['status'] = AdminStatus::tryFrom($this->status);
         if (is_null($data['status'])) unset($data['status']);
+        if (is_string($this->userType)) $data['userType'] = AdminStatus::tryFrom($this->status);
+        if (is_null($data['userType'])) unset($data['userType']);
 
-        unset($data['external_id']);
+        unset($data['id']);
 
-        return $data;
+        return $this->toSnakeCase($data);
     }
 }
